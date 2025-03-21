3fde0f2bb13e21727badfa9373c7e3fedd6dc061
ogasky <onoseogaga@gmail.com>
2025-03-21 15:56:09 +0100

    smart contract deployed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client"; // ✅ Use 'react-dom/client'
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { TransactionProvider } from './context/TransactionContext';
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // ✅ Correct way in React 18+
+root.render(
+  <TransactionProvider>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+  </TransactionProvider>,
+);
+
